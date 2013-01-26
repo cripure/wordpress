a5e448cf020b4f0836328e66bda8e8e920e8f22c
cripure <notrecripure@gmail.com>
2013-01-26 22:20:16 +0900

    1st commit after having set up wp and wiki plugin.

---
+++ wp-content/themes/commons/functions.php
@@ -0,0 +1,3 @@
+<?php
+add_filter( 'twentyeleven_header_image_width' , create_function( '' , 'return 900;' ) );
+add_filter( 'twentyeleven_header_image_height', create_function( '' , 'return 230;' ));
